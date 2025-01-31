2f1f3c1ca87527d6bc4b055a7a9de892328a0300
taehyung0519 <taehyung0519@naver.com>
2025-01-31 13:57:05 +0900

    초기 push

---
+++ WpfBasic/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if(frm.CanGoBack) frm.GoBack();
+        }
+
+        private void Front_Click(object sender, RoutedEventArgs e)
+        {
+            if(frm.CanGoForward) frm.GoForward();   
+        }
+    }
+}
