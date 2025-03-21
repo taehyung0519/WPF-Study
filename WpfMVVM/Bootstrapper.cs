3928f96fa5546a5a30938356de69b3456bb1a7ec
taehyung0519 <taehyung0519@naver.com>
2025-03-21 23:14:24 +0900

    WpfMVVM 플젝 추가

---
+++ WpfMVVM/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using WpfMVVM.ViewModels;
+
+namespace WpfMVVM
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await DisplayRootViewForAsync(typeof(ShellViewModel));
+        }
+    }
+}
---
+++ WpfMVVM/ViewModels/ShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVM.ViewModels
+{
+    class ShellViewModel : PropertyChangedBase
+    {
+        public string btnTxt = "button1";
+        public string Btntxt
+        {
+            get { return btnTxt; }
+            set
+            {
+                btnTxt = value;
+                NotifyOfPropertyChange(() => btnTxt);
+            }
+        }
+    }
+}
